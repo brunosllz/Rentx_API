b8c2c454e4ccf98c5ec26364004ad25eef9945bc
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-06-13 20:52:12 -0300

    Alter name function create specification

--- src/database/migrations/1655163682360-CreateSpecitions.ts
+++ src/database/migrations/1655163682360-CreateSpecitions.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateCatogories1655163073312 implements MigrationInterface {
+export class CreateSpecifications1655163073312 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
