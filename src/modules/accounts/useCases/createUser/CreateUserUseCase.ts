29497abeef604a48f8044c02886de67a1aef64c7
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-06-17 00:44:13 -0300

    Refactoring User use case and repository

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from 'bcryptjs';
 import { ICreateUserDTO } from "../../dto/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { AppError } from "../../../../errors/AppError";
 
 @injectable()
 class CreateUsersUseCase {
@@ -11,9 +13,21 @@ class CreateUsersUseCase {
 
   async execute({ name, email, password, driver_license, avatar }: ICreateUserDTO) {
 
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
+    if (userAlreadyExists) {
+      throw new AppError('User already exists');
+    }
 
-    await this.usersRepository.create({ name, email, password, driver_license, avatar });
+    const hashedPassword = await hash(password, 8);
+
+    await this.usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+      driver_license,
+      avatar
+    });
   }
 }
 
