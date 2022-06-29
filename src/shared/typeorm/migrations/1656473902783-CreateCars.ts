7a93dea9120d6746534597f23c16b43e51a2834a
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-06-29 20:42:03 -0300

    Create migration cars table

---
+++ src/shared/typeorm/migrations/1656473902783-CreateCars.ts
@@ -0,0 +1,70 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCars1656473902783 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "cars",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                    },
+                    {
+                        name: "daily_rate",
+                        type: "numeric"
+                    },
+                    {
+                        name: "available",
+                        type: "boolean",
+                        default: true
+                    },
+                    {
+                        name: "license_plate",
+                        type: "varchar",
+                    },
+                    {
+                        name: "fine_amount",
+                        type: "numeric",
+                    },
+                    {
+                        name: "brand",
+                        type: "varchar",
+                    },
+                    {
+                        name: "category_id",
+                        type: "uuid"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: "FK_category_car",
+                        referencedTableName: "categories",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["category_id"],
+
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
